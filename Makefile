dcb7453a10f93ca2d4a7584aebc9355267523afe
Davy Landman <davy.landman@swat.engineering>
2020-04-11 23:12:29 +0200

    Added tests and initial makefile

---
+++ Makefile
@@ -0,0 +1,32 @@
+.PHONY: clean test dist test-dist
+
+src = $(wildcard src/*.c) $(wildcard src/chacha-portable/*.c) $(wildcard src/poly1305-donna/*.c)
+
+CFLAGS+=-std=gnu99 -pedantic -Wall -Wextra -O2 -Isrc -Isrc/chacha-portable -Isrc/poly1305-donna
+LDFLAGS = 
+
+VERSION?=dev-version
+
+bin/test-vectors: $(src) test/test-vectors.c
+	mkdir -p bin
+	$(CC) -o $@ $^ $(LDFLAGS) $(CFLAGS)
+
+
+bin/test-roundtrip: $(src) test/roundtrip.c
+	mkdir -p bin
+	$(CC) -o $@ $^ $(LDFLAGS) $(CFLAGS)
+
+test: bin/test-roundtrip bin/test-vectors
+	./bin/test-vectors
+	./bin/test-roundtrip
+
+clean:
+	rm -f bin/*
+
+unused: $(src) test/test-roundtrip.c
+	$(CC) -o bin/unused $^ $(LDFLAGS) $(CFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections,--print-gc-sections
+
+test-windows: 
+	docker run --rm -v "/$(PWD):/app" silkeh/clang bash -c 'cd /app && CC=clang CFLAGS="-fsanitize=undefined" make clean test'
+
+test-linux: test test-dist
---
+++ test/roundtrip.c
@@ -0,0 +1,89 @@
+#include "../src/portable8439.h"
+#include "../src/chacha-portable/chacha-portable.h"
+#include "../src/chachar-avr/chacha.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "pcg_random.h"
+
+static void fill_crappy_random(void* target, size_t length, pcg32_random_t* rng) {
+    if (length >= sizeof(uint32_t)) {
+        uint32_t *p = target;
+        uint32_t *last = p + (length / sizeof(uint32_t));
+        while (p < last) {
+            *p++ = pcg32_random_r(rng);
+        }
+    }
+    size_t tail_size = length % sizeof(uint32_t);
+    if (tail_size != 0) {
+        uint32_t tail = pcg32_random_r(rng);
+        memcpy((((uint8_t*)target) + length) - tail_size, &tail, tail_size);
+    }
+}
+
+static void print_hex(const uint8_t *dt, size_t len) {
+    for (int i = 0; i < len; i++) {
+        if (i > 0)
+            printf("");
+        printf("%02X", dt[i]);
+    }
+    printf("\n");
+}
+
+
+struct chacha20_testvector {
+    uint8_t key[32];
+    uint8_t nonce[12];
+    uint8_t resulting_keystream[512];
+    int keystream_check_size;
+};
+
+
+#define MAX_TEST_SIZE (4096)
+int test8439(pcg32_random_t* rng) {
+    uint8_t plain[MAX_TEST_SIZE] = { 0};
+    uint8_t ad[MAX_TEST_SIZE] = { 0 };
+    uint8_t buffer[MAX_TEST_SIZE] = { 0 };
+    uint8_t buffer2[MAX_TEST_SIZE] = { 0 };
+    uint8_t key[RFC_8439_KEY_SIZE] = { 0 };
+    uint8_t nonce[RFC_8439_NONCE_SIZE] = { 0 };
+
+    fill_crappy_random(plain, MAX_TEST_SIZE, rng);
+    fill_crappy_random(ad, MAX_TEST_SIZE, rng);
+    fill_crappy_random(key, RFC_8439_KEY_SIZE, rng);
+    fill_crappy_random(nonce, RFC_8439_NONCE_SIZE, rng);
+
+    for (int i = 0; i < MAX_TEST_SIZE; i++) {
+        uint8_t mac[RFC_8439_MAC_SIZE] = { 0 };
+        portable_chacha20_poly1305_encrypt(mac, buffer, key, nonce, ad, i, plain, i);
+        if (!portable_chacha20_poly1305_decrypt(buffer2, key, nonce, ad, i, mac, buffer2, i)) {
+            printf("Failed encrypting %d bytes\n", i);
+            return 1;
+        }
+        if (memcmp(buffer2, plain, i) != 0) {
+            printf("Failed encrypting %d bytes\n", i);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(void) {
+    srand(time(NULL)); 
+    pcg32_random_t rng;
+    rng.state = rand();
+    rng.inc = rand() | 1;
+    int result = 0;
+    for (int i = 0; i < 20; i++) {
+        result += test8439(&rng);
+    }
+    if (result == 0) {
+        printf("RFC8439 internal round trip success\n");
+    }
+    else {
+        printf("Failed %i times\n", result);
+    }
+    return result;
+}
