3ec19f4793e976ade8be6e8360c8daf231dee9f6
Davy Landman <davy.landman@swat.engineering>
2020-04-18 20:40:03 +0200

    Cleaning warnings

--- test/test-vectors.c
+++ test/test-vectors.c
@@ -14,8 +14,6 @@
 
 static void print_hex(const uint8_t *dt, size_t len) {
     for (size_t i = 0; i < len; i++) {
-        if (i > 0)
-            printf("");
         printf("%02X", dt[i]);
     }
     printf("\n");
