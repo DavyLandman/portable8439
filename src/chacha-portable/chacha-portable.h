999ec773ad04a20b20d68e2961b7d464391e4f8e
Davy Landman <davy.landman@swat.engineering>
2020-04-28 20:19:18 +0200

    A bit more portability

--- src/chacha-portable/chacha-portable.h
+++ src/chacha-portable/chacha-portable.h
@@ -1,6 +1,12 @@
 #ifndef CHACHA_PORTABLE_H 
 #define CHACHA_PORTABLE_H
 
+#if !defined(__cplusplus) && \
+    !defined(_MSC_VER) && \
+    (!defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L)
+#    error "C99 or newer required"
+#endif
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -8,8 +14,18 @@
 #    error "Systems without native octals not suppoted"
 #endif
 
+
 #define CHACHA20_KEY_SIZE (32)
 #define CHACHA20_NONCE_SIZE (12)
+
+#if defined(_MSC_VER) || defined(__cplusplus) 
+// add restrict support
+#    if (defined(_MSC_VER) && _MSC_VER >= 1900) || defined(__clang__) || defined(__GNUC__)
+#       define restrict __restrict
+#    else
+#       define restrict
+#    endif
+#endif
 
 // xor data with a ChaCha20 keystream as per RFC8439
 void chacha20_xor_stream(
