b2eea1e0b214b5e6f2f9132ff2d3f22c33c90de1
Davy Landman <davy.landman@swat.engineering>
2020-06-29 16:20:22 +0200

    Rewrote the algamize file a bit

--- algamize.sh
+++ algamize.sh
@@ -53,7 +53,8 @@ function add_decl_spec() {
         -e 's/^PORTABLE_8439_DECL typedef/typedef/'
 }
 
-echo "// portable8439 $VERSION
+{
+    echo "// portable8439 $VERSION
 // Source: https://github.com/DavyLandman/portable8439
 // Licensed under CC0-1.0
 // Contains poly1305-donna e6ad6e091d30d7f4ec2d4f978be1fcfcbce72781 (Public Domain)
@@ -68,62 +69,63 @@ extern \"C\" {
 #ifndef PORTABLE_8439_DECL
 #define PORTABLE_8439_DECL
 #endif
-" > "$DST_HEADER"
+"
 
-for h in "${PORTABLE_FILES[@]}"; do 
-    cat "$SRC_DIR/$h.h" | remove_header_guard 
-done  | remove_double_blank_lines | add_decl_spec >> "$DST_HEADER" 
+    for h in "${PORTABLE_FILES[@]}"; do 
+        cat "$SRC_DIR/$h.h" | remove_header_guard 
+    done  | remove_double_blank_lines | add_decl_spec 
 
-echo "#if defined(__cplusplus)
+    echo "#if defined(__cplusplus)
 }
 #endif
-#endif" >> "$DST_HEADER"
+#endif"
+} > "$DST_HEADER"
 
-
-echo "// portable8439 $VERSION
+{
+    echo "// portable8439 $VERSION
 // Source: https://github.com/DavyLandman/portable8439
 // Licensed under CC0-1.0
 // Contains poly1305-donna e6ad6e091d30d7f4ec2d4f978be1fcfcbce72781 (Public Domain)
 
-#include \"portable8439.h\"
-" > "$DST_SOURCE"
+#include \"portable8439.h\""
 
-for h in "chacha-portable/chacha-portable" "poly1305-donna/poly1305-donna"; do 
-    echo "// ******* BEGIN: $h.h ********"
-    cat "$SRC_DIR/$h.h" | remove_header_guard | \
-        remove_local_imports | remove_double_blank_lines | \
-        make_everything_static | add_decl_spec
-    echo "// ******* END:   $h.h ********"
-done >> "$DST_SOURCE"
+    for h in "chacha-portable/chacha-portable" "poly1305-donna/poly1305-donna"; do 
+        echo "// ******* BEGIN: $h.h ********"
+        cat "$SRC_DIR/$h.h" | remove_header_guard | \
+            remove_local_imports | remove_double_blank_lines | \
+            make_everything_static | add_decl_spec
+        echo "// ******* END:   $h.h ********"
+    done
 
-function inline_src() {
-    remove_local_imports | \
-    remove_double_blank_lines | \
-    make_everything_static # | \
-    #add_decl_spec
-}
+    function inline_src() {
+        remove_local_imports | \
+        remove_double_blank_lines | \
+        make_everything_static # | \
+        #add_decl_spec
+    }
 
-echo "// ******* BEGIN: chacha-portable.c ********" >> "$DST_SOURCE"
-inline_src <"$SRC_DIR/chacha-portable/chacha-portable.c" >> "$DST_SOURCE"
-echo "// ******* END: chacha-portable.c ********" >> "$DST_SOURCE"
-
-
-DONNA_ROOT="$SRC_DIR/poly1305-donna"
-function merge_donna_src() {
-    awk '
-    /#.*include "poly1305-donna-[0-9]+.h"/ { system("cat src/poly1305-donna/"$3); next; }
-    42
-    ' "$DONNA_ROOT/poly1305-donna.c"
-}
-
-echo "// ******* BEGIN: poly1305-donna.c ********" >> "$DST_SOURCE"
-merge_donna_src | inline_src >> "$DST_SOURCE"
-echo "// ******* END: poly1305-donna.c ********" >> "$DST_SOURCE"
+    echo "// ******* BEGIN: chacha-portable.c ********"
+    inline_src <"$SRC_DIR/chacha-portable/chacha-portable.c"
+    echo "// ******* END:   chacha-portable.c ********"
 
 
-for h in "${PORTABLE_FILES[@]}"; do 
-    echo "// ******* BEGIN: $h.c ********"
-    cat "$SRC_DIR/$h.c" | remove_local_imports | \
-    remove_double_blank_lines | add_decl_spec
-    echo "// ******* END:   $h.c ********"
-done >> "$DST_SOURCE"
+    DONNA_ROOT="$SRC_DIR/poly1305-donna"
+    function merge_donna_src() {
+        awk '
+        /#.*include "poly1305-donna-[0-9]+.h"/ { system("cat src/poly1305-donna/"$3); next; }
+        42
+        ' "$DONNA_ROOT/poly1305-donna.c"
+    }
+
+    echo "// ******* BEGIN: poly1305-donna.c ********"
+    merge_donna_src | inline_src
+    echo "// ******* END:   poly1305-donna.c ********"
+
+
+    for h in "${PORTABLE_FILES[@]}"; do 
+        echo "// ******* BEGIN: $h.c ********"
+        cat "$SRC_DIR/$h.c" | remove_local_imports | \
+        remove_double_blank_lines | add_decl_spec
+        echo "// ******* END:   $h.c ********"
+    done
+} > "$DST_SOURCE"
